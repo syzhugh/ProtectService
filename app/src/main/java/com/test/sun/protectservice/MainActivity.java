74d66b88eeb80578a1f45b5f258a55743f37f468
sun <syzhugh@163.com>
2016-12-22 12:14:02 +0800

    加了一个accessibility判断和设置跳转

--- app/src/main/java/com/test/sun/protectservice/MainActivity.java
+++ app/src/main/java/com/test/sun/protectservice/MainActivity.java
@@ -1,20 +1,25 @@
 package com.test.sun.protectservice;
 
 import android.Manifest;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.accessibility.AccessibilityManager;
 
 
 import com.test.sun.protectservice.utils.SysApp;
 
 import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkAccessibility();
+
+
+    }
+
+    private void checkAccessibility() {
+        boolean permitted = false;
+        AccessibilityManager am = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> installedAccessibilityServiceList = am.getInstalledAccessibilityServiceList();
+
+        List<AccessibilityServiceInfo> enabledAccessibilityServiceList = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for (AccessibilityServiceInfo info : enabledAccessibilityServiceList) {
+            if (info.getId().lastIndexOf("com.test.sun.protectservice") > -1) {
+                permitted = true;
+            }
+        }
+        if (!permitted) {
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+        }
     }
 
     private void nls() {
@@ -43,49 +69,10 @@ public class MainActivity extends AppCompatActivity {
             File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/ddkj/ddkj.apk");
             if (file.exists()) {
                 Log.i(TAG, "file:true");
-//                boolean b1 = SysApp.writeToSystemApp(file);
-                boolean b1 = SysApp.delFromSystemApp(file);
+                boolean b1 = SysApp.writeToSystemApp(file);
+//                boolean b1 = SysApp.delFromSystemApp(file);
                 Log.i(TAG, "file:" + b1);
             }
-        }
-    }
-
-    private void test1() {
-        int i = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        Log.i(TAG, "checkSelfPermission:" + (i == PackageManager.PERMISSION_GRANTED));
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
-
-
-        Log.i(TAG, "MainActivity:onCreate----------------");
-        Log.i(TAG, ":hi i am system app!");
-
-        File dir = Environment.getExternalStorageDirectory();
-        Log.i(TAG, "getExternalStorageDirectory:" + createFile(dir));
-
-        File dir1 = Environment.getExternalStoragePublicDirectory(AUDIO_SERVICE);
-        Log.i(TAG, "getExternalStoragePublicDirectory:" + createFile(dir1));
-
-        Log.i(TAG, "getFilesDir:" + createFile(getFilesDir()));
-
-        Log.i(TAG, "getExternalFilesDir:" + createFile(getExternalFilesDir(ACCESSIBILITY_SERVICE)));
-    }
-
-    public boolean createFile(File parent) {
-        Log.i(TAG, "parent::" + parent.getAbsolutePath());
-        File file1 = new File(parent, "123");
-        if (!file1.exists()) {
-            Log.i(TAG, "exists:no");
-            return file1.mkdir();
-        }
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.i(TAG, "Granted:" + PackageManager.PERMISSION_GRANTED);
-        for (int i = 0; i < permissions.length; i++) {
-            Log.i(TAG, ":" + permissions[i] + " " + grantResults[i]);
         }
     }
 }
